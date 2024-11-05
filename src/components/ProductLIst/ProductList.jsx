efe3f38ada34a2ca40da7e61be3e91897c9548a8
Victor Yrman <vitiayrashevich1256@gmail.com>
2024-11-06 00:54:57 +0300

    Thirteenth commit

--- src/components/ProductLIst/ProductList.jsx
+++ src/components/ProductLIst/ProductList.jsx
@@ -1,8 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductList.css";
+import ProductItem from "../ProductItem/ProductItem";
+import { useTelegram } from "../../hooks/useTelegram";
+
+const products = [
+  {
+    id: "1",
+    title: "Джинсы",
+    price: 5000,
+    description: "Синего цвета, прямые",
+  },
+  {
+    id: "2",
+    title: "Куртка",
+    price: 12000,
+    description: "Красного цвета, осенняя",
+  },
+  {
+    id: "3",
+    title: "Джинсы 2",
+    price: 6000,
+    description: "Серого цвета, зауженные",
+  },
+  {
+    id: "4",
+    title: "Джинсы 3",
+    price: 7000,
+    description: "Красного цвета, широкие",
+  },
+];
+
+const getTotalPrice = (items = []) => {
+  return items.reduce((acc, item) => {
+    return (acc += item.price);
+  });
+};
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const [addedItems, setAddedItems] = useState([]);
+  const { telegram } = useTelegram();
+
+  const onAdd = (product) => {
+    const alreadyAdded = addedItems.find((item) => item.id === product.id);
+    let newItems = [];
+
+    if (alreadyAdded) {
+      newItems = addedItems.filter((item) => item.id !== product.id);
+    } else {
+      newItems = [...addedItems, product];
+    }
+
+    setAddedItems(newItems);
+
+    if (newItems.length === 0) {
+      telegram.MainButton.hide();
+    } else {
+      telegram.MainButton.show();
+      telegram.MainButton.setParams({
+        text: `Купить ${getTotalPrice(newItems)}`,
+      });
+    }
+  };
+
+  return (
+    <div className={"list"}>
+      {products.map((item) => (
+        <ProductItem product={item} onAdd={onAdd} className={"item"} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
